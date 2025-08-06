b303f60b943566a1d80c5f81e7126762a2b43dad
HumaNisar <humajamil200@gmail.com>
2025-08-06 14:13:04 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+// Class definition
+class Sum {
+    int num1;
+    int num2;
+
+    // Method to calculate and display sum
+    void add() {
+        int result = num1 + num2;
+        System.out.println("Sum: " + result);
+    }
+}
+
+// Main class to run the program
+public class Main {
+    public static void main(String[] args) {
+        // Create object of class Sum
+        Sum obj = new Sum();
+
+        // Assign values
+        obj.num1 = 10;
+        obj.num2 = 20;
+
+        // Call the method
+        obj.add();
+    }
+}
